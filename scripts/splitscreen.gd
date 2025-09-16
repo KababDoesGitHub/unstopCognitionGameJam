c604b1c7dd01c18faca0092c53b8b3e70e91821b
Kabir Goswami <kabirmgos1278@gmail.com>
2025-09-16 16:55:54 +0530

    Added Splitscreeen Functionality
    FUCK PARALLAX BACKGROUNDS

---
+++ scripts/splitscreen.gd
@@ -0,0 +1,25 @@
+extends Node
+
+@onready var players := {
+	"1": {
+		viewport = $SubViewportContainer/SubViewport,
+		camera = $SubViewportContainer/SubViewport/Camera2D,
+		player = $SubViewportContainer/SubViewport/Game/player
+	},
+	"2": {
+		viewport = $SubViewportContainer2/SubViewport,
+		camera = $SubViewportContainer2/SubViewport/Camera2D2,
+		player = $SubViewportContainer/SubViewport/Game/player2
+	}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	players["2"].viewport.world_2d = players["1"].viewport.world_2d
+	for node in players.values():
+		var remote_transform := RemoteTransform2D.new()
+		remote_transform.remote_path = node.camera.get_path()
+		node.player.add_child(remote_transform)
+
+func _process(delta: float) -> void:
+	pass
